3febe23ead3f133aa10a04749624e796c9ca7dc7
Fernando Nogueira <fernandojnn@gmail.com>
2016-08-19 10:25:51 -0300

    queue

---
+++ processor/queue_worker.go
@@ -0,0 +1,38 @@
+package processor
+
+import "log"
+
+// Worker represents the worker that executes the job
+type Worker struct {
+	WorkerPool chan chan Job
+	JobChannel chan Job
+	quit       chan bool
+}
+
+func (w Worker) Start() {
+	go func() {
+		for {
+			// register the current worker into the worker queue.
+			w.WorkerPool <- w.JobChannel
+
+			select {
+			case job := <-w.JobChannel:
+			// we have received a work request.
+				if err := job.Payload.UploadToS3(); err != nil {
+					log.Errorf("Error uploading to S3: %s", err.Error())
+				}
+
+			case <-w.quit:
+			// we have received a signal to stop
+				return
+			}
+		}
+	}()
+}
+
+// Stop signals the worker to stop listening for work requests.
+func (w Worker) Stop() {
+	go func() {
+		w.quit <- true
+	}()
+}
